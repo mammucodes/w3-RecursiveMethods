331d6568d261f10e058b5babdb911ee768cce206
Mamatha Pudukutapu <mammucodes@gmail.com>
2023-12-28 23:36:09 -0600

    Ass1-FibonacciRecursive

---
+++ src/main/java/com/w3/recursive/Factorial.java
@@ -0,0 +1,7 @@
+package com.w3.recursive;
+
+public class Factorial {
+    public int  getFactorial(int n){
+        return 1;
+    }
+}
---
+++ src/test/java/com/w3/recursivetests/FibonacciRecursiveTest.java
@@ -0,0 +1,24 @@
+package com.w3.recursivetests;
+
+
+import com.w3.recursive.FibonacciRecursive;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FibonacciRecursiveTest {
+    @Test
+    public void FibonacciRecursiveTest() throws Exception {
+        int n =4;
+        FibonacciRecursive fbr = new FibonacciRecursive();
+        int result =  fbr.getFibonacciNumber(n);
+        Assertions.assertEquals(3,result);
+    }
+    @Test
+    public void FibRecurssiveNegativeNumberTest(){
+        int n = -5;
+        FibonacciRecursive fbr = new FibonacciRecursive();
+        Assertions.assertThrows(Exception.class,()->fbr.getFibonacciNumber(n));
+    }
+
+
+}
